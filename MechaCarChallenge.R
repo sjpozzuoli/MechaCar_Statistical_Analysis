d7611f341d342a9a5ab63fd24459fc992c114102
Stephen Pozzuoli <stephenpozzuoli@gmail.com>
2021-08-01 15:09:07 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,14 +1,24 @@
 install.packages("tidyverse")
 install.packages("jsonlite")
 
-# Load dplyr package
+# Load packages
 library(dplyr)
+library(tidyverse)
 
 # Import and Read in MechaCar_mpg.csv as a DF
 mechacar_mpg_df <- read.csv(file='Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
-#generate multiple linear regression model
+# Generate multiple linear regression model
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg_df) 
 
-#generate summary statistics
+# Generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg_df)) 
+
+# Import and Read in Suspension_Coil.csv as a table
+suspension_coil <- read.csv(file='Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Create a total_summary DF using summarize() function
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups= 'keep')
+
+# Create a lot_summary DF using group_by() and summarize() functions 
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups= 'keep')
