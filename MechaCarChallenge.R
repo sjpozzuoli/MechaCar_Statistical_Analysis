2eb340f5dafc53230e9371d393cfe536a23fb287
Stephen Pozzuoli <stephenpozzuoli@gmail.com>
2021-08-01 14:39:38 -0400

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,14 @@
+install.packages("tidyverse")
+install.packages("jsonlite")
+
+# Load dplyr package
+library(dplyr)
+
+# Import and Read in MechaCar_mpg.csv as a DF
+mechacar_mpg_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+#generate multiple linear regression model
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg_df) 
+
+#generate summary statistics
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg_df)) 
